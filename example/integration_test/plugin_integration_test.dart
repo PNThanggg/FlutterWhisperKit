fece4e3505ab9bdcddbe79c3119bd77ef7782708
PNThanggg <p.n.thang0101@gmail.com>
2025-08-27 22:24:36 +0700

    Refactor: Remove unused integration test and update iOS project configuration
    Removed `plugin_integration_test.dart` as it was no longer in use.
    Updated `project.pbxproj` to include new Pods configurations, frameworks, and build phases for both Runner and RunnerTests targets. This includes adding references to Pods xcconfig files and framework files, as well as new shell script build phases for checking Pods manifest lock and embedding Pods frameworks.

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -1,13 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
-import 'package:flutter_whisper_kit/flutter_whisper_kit.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final String? version = await FlutterWhisperKit.getPlatformVersion();
-    expect(version?.isNotEmpty, true);
-  });
 }
